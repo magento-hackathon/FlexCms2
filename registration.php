5d307ea677f60d3e2a0afe64fe30e5fe93e05bd6
Andreas von Studnitz <avs@integer-net.de>
2015-10-24 16:33:44 +0200

    Initial release, first files

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 FireGento e.V. - All rights reserved.
+ * See LICENSE.md bundled with this module for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'FireGento_FlexCms',
+    __DIR__
+);
